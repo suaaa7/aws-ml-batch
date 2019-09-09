a2b957cccee130a271e7647cb09e97b2246a103f
suaaa7 <spuaaa7@gmail.com>
2019-09-09 22:35:49 +0900

    sleep 10 -> sleep 5

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 apex infra apply -target=module.lambda_role -auto-approve
-sleep 10
+sleep 5
 apex deploy \
   --set WEBHOOK_URL=$WEBHOOK_URL \
   --set BUCKET_NAME=$BUCKET_NAME notify-slack
 apex deploy \
   --set WEBHOOK_URL=$WEBHOOK_URL \
   --set BUCKET_NAME=$BUCKET_NAME check-result
-sleep 10
+sleep 5
 apex infra apply -auto-approve
--- python-batch/docker-test.sh
+++ python-batch/docker-test.sh
@@ -4,7 +4,7 @@ docker build \
     -t test \
     --build-arg BUCKET_NAME=Undefined \
     --build-arg WEBHOOK_URL=${WEBHOOK_URL} --no-cache .
-sleep 10
+sleep 5
 docker run -it --rm test
-sleep 10
+sleep 5
 docker rmi test
